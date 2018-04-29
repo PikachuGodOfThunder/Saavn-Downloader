40baf60b97cc992d642fdf43dd95a014d07580c6
Shrimadhav U K <magicteacher95@gmail.com>
2018-04-29 09:07:53 +0530

    spelling mistake

--- saavn-downloader.py
+++ saavn-downloader.py
@@ -37,7 +37,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0'
 }
 
-TG_BOT_TOKEN = sys.arv[1]
+TG_BOT_TOKEN = sys.argv[1]
 botan_token = sys.argv[2]
 botan = Botan()
 
